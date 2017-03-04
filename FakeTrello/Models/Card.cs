4eb2c8fcdb4ffbc52682ffcc14869c68779ea854
abbie <abigail.c.holder@gmail.com>
2017-03-04 10:56:50 -0600

    I really thought I modified more than this...

--- FakeTrello/Models/Card.cs
+++ FakeTrello/Models/Card.cs
@@ -12,5 +12,8 @@ namespace FakeTrello.Models
         public int CardId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        //Auxiliary : given a card instance, return the list it belongs to 
+        public List BelongsTo { get; set; }
     }
 }
