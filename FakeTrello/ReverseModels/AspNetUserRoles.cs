c8c145bfb089bf2b170282816a19446aaecbe530
abbie <abigail.c.holder@gmail.com>
2017-03-06 20:01:47 -0600

    lecture notes

---
+++ FakeTrello/ReverseModels/AspNetUserRoles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{   //This table doesn't really exist, but the relationship does. (???)
+    public class AspNetUserRoles
+    {
+        [Key]
+        [MaxLength(128)]
+        public string UserId { get; set; }
+        [Key]
+        [MaxLength(128)]
+        public string RoleId { get; set; }
+    }
+}
