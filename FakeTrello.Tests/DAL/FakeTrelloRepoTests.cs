11c887a8dcfcf0b450cca39298615421ed049702
abbie <abigail.c.holder@gmail.com>
2017-03-13 18:56:39 -0500

    in class notes

---
+++ FakeTrello.Tests/DAL/FakeTrelloRepoTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeTrello.DAL;
+
+namespace FakeTrello.Tests.DAL
+{
+    [TestClass]
+    public class FakeTrelloRepoTests
+    {
+
+        [TestMethod]
+        public void EnsureICanCreateInstanceOfRepo()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureIHaveNotNullContext()
+        {
+            FakeTrelloRepository repo = new FakeTrelloRepository();
+
+            Assert.IsNotNull(repo.Context);
+        }
+
+        [TestMethod]
+        public void EnsureICanInjectContextInstance()
+        {
+            FakeTrelloContext context = new FakeTrelloContext();
+            FakeTrelloRepository repo = new FakeTrelloRepository(context);
+
+            Assert.IsNotNull(repo.Context);
+        }
+    }
+}
